2597a59aafa8b1b267f08abf7642ab18404678ef
Nishant Popli <nishantpopli1234@gmail.com>
2024-11-24 20:51:41 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import Hero from './components/Hero';
 import About from './components/About';
 import NavBar from './components/Navbar';
 import Features from './components/Features';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Story from './components/Story'
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <NavBar/>
-      <Hero/>
-      <About/>
-      <Features/>
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story/>
+      <Contact />
+      <Footer />
     </main>
   );
 }
